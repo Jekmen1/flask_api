d877e541045f4eaae66d9a25b7c94beba920ed25
Jekmen1 <jeko.kobakidze@gmail.com>
2024-08-02 16:11:50 +0400

    add post method

--- api.py
+++ api.py
@@ -1,9 +1,18 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+todos = {
+     1: {'task': 'write code', 'summary': 'using pycharm'},
+     2: {'task': 'task 2', 'summary': 'using pycharm'},
+     3: {'task': 'task 3', 'summary': 'using pycharm'}
+}
+
+task_post_args = reqparse.RequestParser()
+task_post_args.add_argument("task", type=str, help='Task is required',required=True)
+task_post_args.add_argument("summary", type=str, help='summary is required',required=True)
 
 class HelloWorld(Resource):
     def get(self):
@@ -13,6 +22,25 @@ class HelloName(Resource):
     def get(self, name):
         return {'data': 'Hello, {}'.format(name)}
 
+class Todo(Resource):
+    def get(self, todo_id):
+        return todos[todo_id]
+    def post(self, todo_id):
+        args = task_post_args.parse_args()
+        if todo_id in todos:
+            abort(409)
+        todos[todo_id] = {"task": args["task"], "summary": args['summary']}
+        return todos
+
+class TodoList(Resource):
+    def get(self):
+        return todos
+
+
+
+
+api.add_resource(Todo, '/todos/<int:todo_id>')
+api.add_resource(TodoList, '/todos')
 api.add_resource(HelloWorld, '/helloworld')
 api.add_resource(HelloName, '/helloworld/<string:name>')
 
