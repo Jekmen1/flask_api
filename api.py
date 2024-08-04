d30adf4ca429136f96a0680eac8f09849e0007ab
Jekmen1 <jeko.kobakidze@gmail.com>
2024-08-04 18:55:36 +0400

    add sqlalchemy

--- api.py
+++ api.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse, abort
+from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+db = SQLAlchemy(app)
 
-todos = {
-    1: {'task': 'write code', 'summary': 'using pycharm'},
-    2: {'task': 'task 2', 'summary': 'using pycharm'},
-    3: {'task': 'task 3', 'summary': 'using pycharm'}
-}
+
+class ToDoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200))
+    summary = db.Column(db.String(500))
+
+
+
+
+def create_tables():
+    db.create_all()
+
 
 task_post_args = reqparse.RequestParser()
 task_post_args.add_argument("task", type=str, help='Task is required', required=True)
@@ -18,56 +28,67 @@ task_put_args = reqparse.RequestParser()
 task_put_args.add_argument("task", type=str, help='Task is required')
 task_put_args.add_argument("summary", type=str, help='Summary is required')
 
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'data': 'Hello, World!'}
-
-
-class HelloName(Resource):
-    def get(self, name):
-        return {'data': 'Hello, {}'.format(name)}
-
-
-class Todo(Resource):
-    def get(self, todo_id):
-        if todo_id not in todos:
-            abort(404, message="Todo ID {} doesn't exist".format(todo_id))
-        return todos[todo_id]
-
-    def post(self, todo_id):
-        args = task_post_args.parse_args()
-        if todo_id in todos:
-            abort(409, message="Todo ID {} already exists".format(todo_id))
-        todos[todo_id] = {"task": args["task"], "summary": args['summary']}
-        return todos[todo_id], 201
-
-    def put(self, todo_id):
-        args = task_put_args.parse_args()
-        if todo_id not in todos:
-            abort(404, message="Todo ID {} doesn't exist".format(todo_id))
-        if args['task']:
-            todos[todo_id]['task'] = args['task']
-        if args['summary']:
-            todos[todo_id]['summary'] = args['summary']
-        return todos[todo_id]
-
-    def delete(self, todo_id):
-        if todo_id not in todos:
-            abort(404, message="Todo ID {} doesn't exist".format(todo_id))
-        del todos[todo_id]
-        return '', 204
+resource_fields = {
+    'id': fields.Integer,
+    'task': fields.String,
+    'summary': fields.String
+}
 
 
 class TodoList(Resource):
+    @marshal_with(resource_fields)
     def get(self):
-        return todos
+        tasks = ToDoModel.query.all()
+        return tasks
 
 
-api.add_resource(Todo, '/todos/<int:todo_id>')
+class ToDo(Resource):
+    @marshal_with(resource_fields)
+    def get(self, todo_id):
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if not task:
+            abort(404, message="Todo ID {} doesn't exist".format(todo_id))
+        return task
+
+    @marshal_with(resource_fields)
+    def post(self, todo_id):
+        args = task_post_args.parse_args()
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if task:
+            abort(409, message="Todo ID {} already exists".format(todo_id))
+
+        todo = ToDoModel(id=todo_id, task=args['task'], summary=args['summary'])
+        db.session.add(todo)
+        db.session.commit()
+        return todo, 201
+
+    @marshal_with(resource_fields)
+    def put(self, todo_id):
+        args = task_put_args.parse_args()
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if not task:
+            abort(404, message="Todo ID {} doesn't exist".format(todo_id))
+
+        if args['task']:
+            task.task = args['task']
+        if args['summary']:
+            task.summary = args['summary']
+
+        db.session.commit()
+        return task
+
+    @marshal_with(resource_fields)
+    def delete(self, todo_id):
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if not task:
+            abort(404, message="Todo ID {} doesn't exist".format(todo_id))
+        db.session.delete(task)
+        db.session.commit()
+        return '', 204
+
+
+api.add_resource(ToDo, '/todos/<int:todo_id>')
 api.add_resource(TodoList, '/todos')
-api.add_resource(HelloWorld, '/helloworld')
-api.add_resource(HelloName, '/helloworld/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
